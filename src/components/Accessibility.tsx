6c6f0dc81d04dab53925c92f76a8fc9f229dcadf
richardJET <richardtillo@gmail.com>
2023-09-12 00:14:38 -0400

    fixed browser compaitbility issues

--- src/components/Accessibility.tsx
+++ src/components/Accessibility.tsx
@@ -171,7 +171,7 @@ export default function Accessibility() {
 
     return (
         <>
-            <button className="btn btn-success position-fixed rounded-circle end-0 m-2" data-bs-toggle="modal" data-bs-target="#accessibilityModal" style={medium}>
+            <button className="btn btn-success position-fixed rounded-circle end-0 m-2 z-3" data-bs-toggle="modal" data-bs-target="#accessibilityModal" style={medium}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 57" className="text-white" fill="white" style={small}><path xmlns="http://www.w3.org/2000/svg" d="M20.821 0a5.364 5.364 0 1 0 .001 10.727A5.364 5.364 0 0 0 20.821 0zm-2.889 11.083c-3.158.007-3.741 1.995-3.741 3.622v19.138a2.96 2.96 0 0 0 2.969 2.969l13.597-.06s4.681 7.089 7.026 10.628c.703 1.061 1.638 1.999 2.988 1.999 1.781 0 3.421-1.453 3.226-3.226-.074-.695-.231-1.354-.652-1.979-2.091-3.117-6.234-9.859-8.511-12.39-.578-.643-1.377-1.112-2.315-1.109l-11.242.04V18.802l8.016 5.542a2.527 2.527 0 0 0 3.522-.653 2.526 2.526 0 0 0-.653-3.523l-12.508-8.629c-.434-.3-1.236-.456-1.722-.456zm30.994 5.364a.75.75 0 0 0-.535.217l-9.995 9.995H24.225a.748.748 0 0 0-.751.752v.277c0 .419.333.752.751.752h14.567a.832.832 0 0 0 .573-.217l10.292-10.292a.756.756 0 0 0 0-1.069l-.198-.198a.75.75 0 0 0-.533-.217zm-36.16 5.225C5.423 23.879 0 30.721 0 38.949 0 48.995 8.121 57 17.892 57c7.486 0 14.004-4.687 16.645-11.4l-2.691-4.058c-1.22 6.446-6.914 11.421-13.953 11.421-7.955 0-14.21-6.363-14.21-14.014 0-5.9 3.735-11.026 9.084-13.062v-4.215z"></path></svg>
             </button>
             <div className="modal fade" id="accessibilityModal">
@@ -242,22 +242,22 @@ export default function Accessibility() {
                                 <h4 className="mb-4">Content Adjustments</h4>
                                 <div className="row">
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${alignLeft ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setAlignLeft(!alignLeft)}><FontAwesomeIcon icon={faAlignLeft} size="2xl" className="mb-2" />Align Left</button>
+                                        <button className={`btn ${alignLeft ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setAlignLeft(!alignLeft)}><FontAwesomeIcon icon={faAlignLeft} size="2xl" className="mb-2" />Align Left</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${alignCenter ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setAlignCenter(!alignCenter)}><FontAwesomeIcon icon={faAlignCenter} size="2xl" className="mb-2" />Align Center</button>
+                                        <button className={`btn ${alignCenter ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setAlignCenter(!alignCenter)}><FontAwesomeIcon icon={faAlignCenter} size="2xl" className="mb-2" />Align Center</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${alignRight ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setAlignRight(!alignRight)} ><FontAwesomeIcon icon={faAlignRight} size="2xl" className="mb-2" />Align Right</button>
+                                        <button className={`btn ${alignRight ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setAlignRight(!alignRight)} ><FontAwesomeIcon icon={faAlignRight} size="2xl" className="mb-2" />Align Right</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${highlightTitles ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setHighlightTitles(!highlightTitles)}><FontAwesomeIcon icon={faT} size="2xl" className="mb-2" />Highlight Titles</button>
+                                        <button className={`btn ${highlightTitles ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setHighlightTitles(!highlightTitles)}><FontAwesomeIcon icon={faT} size="2xl" className="mb-2" />Highlight Titles</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${highlightLinks ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setHighlightLinks(!highlightLinks)}><FontAwesomeIcon icon={faLink} size="2xl" className="mb-2" />Highlight Links</button>
+                                        <button className={`btn ${highlightLinks ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setHighlightLinks(!highlightLinks)}><FontAwesomeIcon icon={faLink} size="2xl" className="mb-2" />Highlight Links</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${textMagnifier ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setTextMagnifier(!textMagnifier)}><FontAwesomeIcon icon={faMagnifyingGlass} size="2xl" className="mb-2" />Text Magnifier</button>
+                                        <button className={`btn ${textMagnifier ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setTextMagnifier(!textMagnifier)}><FontAwesomeIcon icon={faMagnifyingGlass} size="2xl" className="mb-2" />Text Magnifier</button>
                                     </div>
                                 </div>
                             </div>
@@ -265,22 +265,22 @@ export default function Accessibility() {
                                 <h4 className="mb-4">Color Adjustments</h4>
                                 <div className="row">
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${darkContrast ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setDarkContrast(!darkContrast)}><FontAwesomeIcon icon={faMoon} size="2xl" className="mb-2" />Dark Contrast</button>
+                                        <button className={`btn ${darkContrast ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setDarkContrast(!darkContrast)}><FontAwesomeIcon icon={faMoon} size="2xl" className="mb-2" />Dark Contrast</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${lightContrast ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setLightContrast(!lightContrast)}><FontAwesomeIcon icon={faSun} size="2xl" className="mb-2" />Light Contrast</button>
+                                        <button className={`btn ${lightContrast ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setLightContrast(!lightContrast)}><FontAwesomeIcon icon={faSun} size="2xl" className="mb-2" />Light Contrast</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${highContrast ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setHighContrast(!highContrast)}><FontAwesomeIcon icon={faCircleHalfStroke} size="2xl" className="mb-2" />High Contrast</button>
+                                        <button className={`btn ${highContrast ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setHighContrast(!highContrast)}><FontAwesomeIcon icon={faCircleHalfStroke} size="2xl" className="mb-2" />High Contrast</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${highSaturation ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setHighSaturation(!highSaturation)}><FontAwesomeIcon icon={faDroplet} size="2xl" className="mb-2" />High Saturation</button>
+                                        <button className={`btn ${highSaturation ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setHighSaturation(!highSaturation)}><FontAwesomeIcon icon={faDroplet} size="2xl" className="mb-2" />High Saturation</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${lowSaturation ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setLowSaturation(!lowSaturation)}><FontAwesomeIcon icon={faEyeDropper} size="2xl" className="mb-2" />Low Saturation</button>
+                                        <button className={`btn ${lowSaturation ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setLowSaturation(!lowSaturation)}><FontAwesomeIcon icon={faEyeDropper} size="2xl" className="mb-2" />Low Saturation</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${monochrome ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setMonochrome(!monochrome)} ><FontAwesomeIcon icon={faCircleHalfStroke} size="2xl" className="mb-2" />Monochrome</button>
+                                        <button className={`btn ${monochrome ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setMonochrome(!monochrome)} ><FontAwesomeIcon icon={faCircleHalfStroke} size="2xl" className="mb-2" />Monochrome</button>
                                     </div>
                                 </div>
                             </div>
@@ -288,22 +288,22 @@ export default function Accessibility() {
                                 <h4 className="mb-4">Orientation Adjustments</h4>
                                 <div className="row">
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${muteSounds ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setMuteSounds(!muteSounds)}><FontAwesomeIcon icon={faVolumeXmark} size="2xl" className="mb-2" />Mute Sounds</button>
+                                        <button className={`btn ${muteSounds ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setMuteSounds(!muteSounds)}><FontAwesomeIcon icon={faVolumeXmark} size="2xl" className="mb-2" />Mute Sounds</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${stopAnimations ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setStopAnimations(!stopAnimations)}><FontAwesomeIcon icon={faBolt} size="2xl" className="mb-2" />Stop Animations</button>
+                                        <button className={`btn ${stopAnimations ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setStopAnimations(!stopAnimations)}><FontAwesomeIcon icon={faBolt} size="2xl" className="mb-2" />Stop Animations</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${readMode ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setReadMode(!readMode)}><FontAwesomeIcon icon={faBook} size="2xl" className="mb-2" />Read Mode</button>
+                                        <button className={`btn ${readMode ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setReadMode(!readMode)}><FontAwesomeIcon icon={faBook} size="2xl" className="mb-2" />Read Mode</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${highlightFocus ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setHighlightFocus(!highlightFocus)}><FontAwesomeIcon icon={faCircleDot} size="2xl" className="mb-2" />Highlight Focus</button>
+                                        <button className={`btn ${highlightFocus ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setHighlightFocus(!highlightFocus)}><FontAwesomeIcon icon={faCircleDot} size="2xl" className="mb-2" />Highlight Focus</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${highlightHover ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setHighlightHover(!highlightHover)}><FontAwesomeIcon icon={faArrowPointer} size="2xl" className="mb-2" />Highlight Hover</button>
+                                        <button className={`btn ${highlightHover ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setHighlightHover(!highlightHover)}><FontAwesomeIcon icon={faArrowPointer} size="2xl" className="mb-2" />Highlight Hover</button>
                                     </div>
                                     <div className="col-6 col-md-4 mb-4">
-                                        <button className={`btn ${hideImages ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center`} onClick={() => setHideImages(!hideImages)}><FontAwesomeIcon icon={faImage} size="2xl" className="mb-2" />Hide Images</button>
+                                        <button className={`btn ${hideImages ? 'btn-success text-white' : 'btn-info'} d-flex flex-column w-100 p-4 h-100 justify-content-center align-items-center`} onClick={() => setHideImages(!hideImages)}><FontAwesomeIcon icon={faImage} size="2xl" className="mb-2" />Hide Images</button>
                                     </div>
                                 </div>
                             </div>
